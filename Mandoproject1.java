f95a98259ee0507b7753e7279930777d45261dc3
Hammam Al Rosyid Mudhoffar <hammam.mudhoffar@gmail.com>
2024-08-27 07:43:49 +0700

    comit ini

---
+++ Mandoproject1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mandoproject1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mandoproject1 {
+
+    public static void main(String[] args) {
+        String nama = "Armando Firlian Ihza Yulianto";
+        String npm = "2310506016";
+        String alamat = "Magelang";
+        String noHp = "0813931792525";
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("NPM : " + npm);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("No HP : " + noHp);
+    }
+}
